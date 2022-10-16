5342e11598d1a66bc0612c8d2193b84be0659730
dntjd960403 <dntjd960403@gmail.com>
2022-10-16 20:35:08 +0900

    회원가입 기능 완성

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,10 +1,13 @@
 const SignupService = require('../services/signup');
 const Joi = require('joi');
 
-const usersSchema = Joi.object({
-  nickname: Joi.string().alphanum().min(3).required(),
-  password: Joi.string().min(4).required(),
-  confirm: Joi.string().required(),
+const re_nickname = /^[a-zA-Z0-9]{3,10}$/;
+const re_password = /^[a-zA-Z0-9]{4,30}$/;
+
+const userSchema = Joi.object({
+  nickname: Joi.string().pattern(re_nickname).required(),
+  password: Joi.string().pattern(re_password).required(),
+  confirm: Joi.string(),
 });
 
 class SignupController {
@@ -12,34 +15,38 @@ class SignupController {
   
     postSignup = async (req, res, next) => {
       try {
-        let { nickname, password, confirm } = await usersSchema.validateAsync(req.body);
+        const { nickname, password, confirm } = await userSchema.validateAsync(req.body);
   
         if (password !== confirm) {
-          res.status(401).send({
-            errorMessage: "패스워드가 패스워드 확인란과 다릅니다.",
-          });
-          return;
-        }
-  
-        if (password.includes(nickname)) {
-          res.status(400).send({
-            errorMessage: "패스워드에 닉네임을 넣을 수 없습니다.",
-          });
-          return;
-        }
+            return res.status(412).send({
+              errorMessage: '패스워드가 일치하지 않습니다.',
+            });
+          }
+          if (nickname.search(re_nickname) === -1) {
+            return res.status(412).send({
+              errorMessage: 'ID의 형식이 일치하지 않습니다.',
+            });
+          }
+          if (password.search(re_password) === -1) {
+            return res.status(412).send({
+              errorMessage: '패스워드 형식이 일치하지 않습니다.',
+            });
+          }
+          if (isRegexValidation(password, nickname)) {
+            return res.status(412).send({
+              errorMessage: '패스워드에 닉네임이 포함되어 있습니다.',
+            });
+          }
   
         const existUsers = await this.signupService.findSameNickname(nickname)
+
         if (existUsers) {
-          res.status(409).send({
-            errorMessage: "이미 가입된 닉네임이 있습니다.",
-          });
-          return;
+            return res.status(412).send({errorMessage: '중복된 닉네임입니다.'});
         } else {
           const createdUser = await this.signupService.createUser(nickname, password);
           
           res.status(201).send({ message: '회원 가입에 성공하였습니다.', data: createdUser});
         }
-  
       } catch (error) {
         const message = `${req.method} ${req.originalUrl} : ${error.message}`;
         console.log(message);
@@ -47,5 +54,9 @@ class SignupController {
       }
     };
   }
-  
+
+  function isRegexValidation(target, regex) {
+    return target.search(regex) !== -1;
+  }
+
   module.exports = SignupController
